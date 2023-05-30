23f23180fae3c11ff394434c871b16810b82f9b7
SeungBin Kim <luxorienskim@gmail.com>
2023-05-30 11:07:43 +0900

    update; hitcounter.mjs to use aws-sdk v3

--- lambda/js/hitcounter.mjs
+++ lambda/js/hitcounter.mjs
@@ -1,27 +1,35 @@
-import { DynamoDB, Lambda } from 'aws-sdk'
+import { DynamoDBClient, UpdateItemCommand } from '@aws-sdk/client-dynamodb'
+import { LambdaClient, InvokeCommand } from '@aws-sdk/client-lambda'
 
 export const handler = async function (event) {
     console.log('request:', JSON.stringify(event, undefined, 2))
 
     // create AWS SDK clients
-    const dynamo = new DynamoDB()
-    const lambda = new Lambda()
+    const dynamo = new DynamoDBClient()
+    const lambda = new LambdaClient()
 
     // update dynamo entry for "path" with hits++
-    await dynamo.updateItem({
-        TableName: process.env.HITS_TABLE_NAME,
-        Key: { path: { S: event.path } },
-        UpdateExpression: 'ADD hits :incr',
-        ExpressionAttributeValues: { ':incr': { N: '1' } }
-    }).promise()
+    await dynamo.send(
+        new UpdateItemCommand({
+            TableName: process.env.HITS_TABLE_NAME,
+            Key: { path: { S: event.path } },
+            UpdateExpression: 'ADD hits :incr',
+            ExpressionAttributeValues: { ':incr': { N: '1' } }
+        })
+    )
 
     // call downstream function and capture response
-    const resp = await lambda.invoke({
-        FunctionName: process.env.DOWNSTREAM_FUNCTION_NAME,
-        Payload: JSON.stringify(event)
-    }).promise()
+    const resp = await lambda.send(
+        new InvokeCommand({
+            FunctionName: process.env.DOWNSTREAM_FUNCTION_NAME,
+            Payload: JSON.stringify(event)
+        })
+    )
 
-    console.log('downstream response:', JSON.stringify(resp, undefined, 2))
+    console.log(
+        'downstream response:',
+        JSON.stringify(resp, undefined, 2)
+    )
 
     // return response back to upstream caller
     return JSON.parse(resp.Payload)
