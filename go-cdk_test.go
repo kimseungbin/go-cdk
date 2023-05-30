a678af13a424e1e2b4630f4c2d92f18a9bd2232d
SeungBin Kim <luxorienskim@gmail.com>
2023-05-30 13:53:48 +0900

    test: remove bootstrap test

--- go-cdk_test.go
+++ go-cdk_test.go
@@ -9,16 +9,6 @@ import (
 	"testing"
 )
 
-// import (
-// 	"testing"
-
-// 	"github.com/aws/aws-cdk-go/awscdk/v2"
-// 	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
-// 	"github.com/aws/jsii-runtime-go"
-// )
-
-// example tests. To run these tests, uncomment this file along with the
-// example resource in go-cdk_test.go
 func TestHitCounterConstruct(t *testing.T) {
 	defer jsii.Close()
 
@@ -40,18 +30,3 @@ func TestHitCounterConstruct(t *testing.T) {
 	template := assertions.Template_FromStack(stack, nil)
 	template.ResourceCountIs(jsii.String("AWS::DynamoDB::Table"), jsii.Number(1))
 }
-
-// func TestGoCdkStack(t *testing.T) {
-// 	// GIVEN
-// 	app := awscdk.NewApp(nil)
-
-// 	// WHEN
-// 	stack := NewGoCdkStack(app, "MyStack", nil)
-
-// 	// THEN
-// 	template := assertions.Template_FromStack(stack)
-
-// 	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
-// 		"VisibilityTimeout": 300,
-// 	})
-// }
