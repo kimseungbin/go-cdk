b59859f495e7453532e64f0a384f46151ad6b5cf
SeungBin Kim <luxorienskim@gmail.com>
2023-05-30 13:53:28 +0900

    test: add TestHitCounterConstruct

--- go-cdk_test.go
+++ go-cdk_test.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+	"github.com/aws/jsii-runtime-go"
+	"go-cdk/hitcounter"
+	"testing"
+)
+
 // import (
 // 	"testing"
 
@@ -10,6 +19,28 @@ package main
 
 // example tests. To run these tests, uncomment this file along with the
 // example resource in go-cdk_test.go
+func TestHitCounterConstruct(t *testing.T) {
+	defer jsii.Close()
+
+	// Given
+
+	stack := awscdk.NewStack(nil, nil, nil)
+
+	// When
+	testFn := awslambda.NewFunction(stack, jsii.String("TestFunc"), &awslambda.FunctionProps{
+		Code:    awslambda.Code_FromAsset(jsii.String("lambda"), nil),
+		Runtime: awslambda.Runtime_NODEJS_16_X(),
+		Handler: jsii.String("hello.handler"),
+	})
+	hitcounter.NewHitCounter(stack, "MyTestConstruct", &hitcounter.HitCounterProps{
+		Downstream: testFn,
+	})
+
+	// Then
+	template := assertions.Template_FromStack(stack, nil)
+	template.ResourceCountIs(jsii.String("AWS::DynamoDB::Table"), jsii.Number(1))
+}
+
 // func TestGoCdkStack(t *testing.T) {
 // 	// GIVEN
 // 	app := awscdk.NewApp(nil)
