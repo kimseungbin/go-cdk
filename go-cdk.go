47e58399efc5737078be7d09c67c4ca4f94d8276
SeungBin Kim <luxorienskim@gmail.com>
2023-05-30 11:59:10 +0900

    feat: implement dynamotableview

--- go-cdk.go
+++ go-cdk.go
@@ -4,6 +4,7 @@ import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+	"github.com/cdklabs/cdk-dynamo-table-viewer-go/dynamotableviewer"
 	"go-cdk/hitcounter"
 
 	"github.com/aws/aws-cdk-go/awscdklambdagoalpha/v2"
@@ -39,6 +40,11 @@ func NewGoCdkStack(scope constructs.Construct, id string, props *GoCdkStackProps
 
 	awsapigateway.NewLambdaRestApi(stack, jsii.String("Endpoint"), &awsapigateway.LambdaRestApiProps{
 		Handler: hitcounter.Handler(),
+	})
+
+	dynamotableviewer.NewTableViewer(stack, jsii.String("ViewHitcounter"), &dynamotableviewer.TableViewerProps{
+		Title: jsii.String("Hello Hits"),
+		Table: hitcounter.Table(),
 	})
 
 	return
--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/cdklabs/awscdk-asset-awscli-go/awscliv1/v2 v2.2.177 // indirect
 	github.com/cdklabs/awscdk-asset-kubectl-go/kubectlv20/v2 v2.1.1 // indirect
 	github.com/cdklabs/awscdk-asset-node-proxy-agent-go/nodeproxyagentv5/v2 v2.0.148 // indirect
+	github.com/cdklabs/cdk-dynamo-table-viewer-go/dynamotableviewer v0.2.463 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/yuin/goldmark v1.4.13 // indirect
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
